76dbbdb07322aad4b18119cc324a2ff4c6e9dd47
Viniciuslb067 <viniciuslb067@gmail.com>
2022-05-21 15:19:22 -0300

    first commit

---
+++ src/main/java/com/ifg/edu/br/chatapi/model/Status.java
@@ -0,0 +1,7 @@
+package com.ifg.edu.br.chatapi.model;
+
+public enum Status {
+    JOIN,
+    MESSAGE,
+    LEAVE
+}
---
+++ src/test/java/com/ifg/edu/br/chatapi/ChatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifg.edu.br.chatapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
